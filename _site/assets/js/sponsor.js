eef496ba29edbb543cc832c30ce2b3422aeacd20
ugfire <ugfirecompany@gmail.com>
2024-07-26 20:58:23 +0300

    added donate to house project

--- _site/assets/js/sponsor.js
+++ _site/assets/js/sponsor.js
@@ -1,10 +1,15 @@
 Swal.fire({
-  title: " Foster/adopt a child?",
+  title: " Build a safe home",
   html: `
-  <h1 class="text-center display-1 "> <span class="bi bi-pen-fill text-warning"></span></h1>    
- 
-  <p><a class="btn btn-light mr-2 m-2" onclick="Swal.close()">Not Yet</a><a class="btn btn-warning m-2" onclick="contact()">Yes please</a></p>
-     
+  <h1 class="text-center display-1 "> <span class="bi bi-house text-dark"></span></h1> 
+  <p class="py-3">Help us build a safe home for a Vulnerable family of 6 who are currently under alternative care at Kids' Club Kampala under Ewafe home.</p>   
+ <p class="p-2 bg-light fw-bold  my-3">Every penny counts </p>
+ <p>Donate via  <span class="fw-bold text-danger"> Airtel Money
+
+
+0200905793  </span> (Wambayo Samuel)</p>
+  <p><a class="btn btn-secondary mr-2 m-2" onclick="Swal.close()">Not Yet</a><a class="btn btn-warning m-2" onclick="contact()">Use Bank</a></p>
+    
     `,
   showCloseButton: true,
   showCancelButton: false,
@@ -16,5 +21,5 @@ function close() {
 }
 
 function contact() {
-  window.location.href = "/contact";
+  window.location.href = "/give";
 }
